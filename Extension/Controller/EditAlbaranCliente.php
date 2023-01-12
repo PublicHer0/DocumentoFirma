fe21e431821ec0c61bc30f270ce77553da2a1925
PublicHer0 <andonimoreno21@gmail.com>
2023-01-12 23:11:57 +0000

    Create extension EditAlbaranCliente

---
+++ Extension/Controller/EditAlbaranCliente.php
@@ -0,0 +1,14 @@
+<?php
+namespace FacturaScripts\Plugins\DocumentoFirma\Extension\Controller;
+
+use Closure;
+
+class EditAlbaranCliente
+{
+    public function createViews(): Closure
+    {
+        return function() {
+            $this->addHtmlView('Firma', 'firma', 'Firma', 'firma','fas fa-code-branch');
+        };
+    }
+}
